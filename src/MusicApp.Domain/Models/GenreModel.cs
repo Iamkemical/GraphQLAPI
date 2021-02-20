76dd3073aa8cbf66b1d1821e343234fe00e2e07d
Gabriel Isaac <iamkemical1@gmail.com>
2021-02-20 12:22:14 +0100

    Added GenreModel

--- src/MusicApp.Domain/Models/GenreModel.cs
+++ src/MusicApp.Domain/Models/GenreModel.cs
@@ -7,6 +7,12 @@ namespace MusicApp.Domain.Models
 {
     public class GenreModel
     {
-        
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public DateTime? DateCreated { get; set; }
     }
 }
