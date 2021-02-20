5aaf7e5b9980620d6bdecc94bc71928d2c667811
Gabriel Isaac <iamkemical1@gmail.com>
2021-02-21 00:55:23 +0100

    Added Connection string and configured dbcontext to service container

--- src/MusicApp.API/MusicApp.API/Data/ApplicationDbContext.cs
+++ src/MusicApp.API/MusicApp.API/Data/ApplicationDbContext.cs
@@ -1,15 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MusicApp.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MusicApp.API.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext()
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
         {
 
         }
+
+        public DbSet<Genre> Genres { get; set; }
     }
 }
