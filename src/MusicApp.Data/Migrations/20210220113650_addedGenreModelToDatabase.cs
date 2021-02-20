d28083c74f17f64eefc27580c08337edec2a5a10
Gabriel Isaac <iamkemical1@gmail.com>
2021-02-20 12:39:43 +0100

    Added migration and updated database

---
+++ src/MusicApp.Data/Migrations/20210220113650_addedGenreModelToDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicApp.Data.Migrations
+{
+    public partial class addedGenreModelToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
