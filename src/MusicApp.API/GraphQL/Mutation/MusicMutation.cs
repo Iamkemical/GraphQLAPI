ca8475a76e620249dc8d2536d6c12f19bc95e89e
Gabriel Isaac <iamkemical1@gmail.com>
2021-02-22 00:00:32 +0100

    Added Create Music Mutation

---
+++ src/MusicApp.API/GraphQL/Mutation/MusicMutation.cs
@@ -0,0 +1,36 @@
+﻿using HotChocolate;
+using HotChocolate.Data;
+using MusicApp.API.Data;
+using MusicApp.API.GraphQL.Musics;
+using MusicApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApp.API.GraphQL.Mutation
+{
+    public class MusicMutation
+    {
+        [UseDbContext(typeof(ApplicationDbContext))]
+        public async Task<CreateMusicPayload> CreateMusicAsync(CreateMusicInput input,
+            [ScopedService] ApplicationDbContext dbContext)
+        {
+            var music = new Music
+            {
+                Name = input.Name,
+                DateCreated = input.DateCreated,
+                Audience = input.Audience,
+                Rating = input.Rating,
+                Picture = input.Picture,
+                GenreId = input.GenreId,
+                SubGenreId = input.SubGenreId
+            };
+
+            dbContext.Musics.Add(music);
+            await dbContext.SaveChangesAsync();
+
+            return new CreateMusicPayload(music);
+        }
+    }
+}
