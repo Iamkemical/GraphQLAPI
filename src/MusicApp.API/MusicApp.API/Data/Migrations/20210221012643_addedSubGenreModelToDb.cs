8218eda11c7dd0f3754d60651bb9fd7cad7852d9
Gabriel Isaac <iamkemical1@gmail.com>
2021-02-21 02:35:25 +0100

    Added SubGenre migration and updated database

---
+++ src/MusicApp.API/MusicApp.API/Data/Migrations/20210221012643_addedSubGenreModelToDb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicApp.API.Migrations
+{
+    public partial class addedSubGenreModelToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SubGenres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    GenreId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubGenres", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubGenres_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubGenres_GenreId",
+                table: "SubGenres",
+                column: "GenreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubGenres");
+        }
+    }
+}
