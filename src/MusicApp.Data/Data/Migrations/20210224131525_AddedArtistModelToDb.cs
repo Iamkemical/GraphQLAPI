85d88ab782dcb6c20d7587ef2002ece2131ceb1a
Gabriel Isaac <iamkemical1@gmail.com>
2021-02-24 14:19:08 +0100

    Added ArtistModel Migration and Updated Database

---
+++ src/MusicApp.Data/Data/Migrations/20210224131525_AddedArtistModelToDb.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicApp.API.Migrations
+{
+    public partial class AddedArtistModelToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    SubGenreId = table.Column<int>(type: "int", nullable: false),
+                    MusicId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Artists_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Artists_Musics_MusicId",
+                        column: x => x.MusicId,
+                        principalTable: "Musics",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Artists_SubGenres_SubGenreId",
+                        column: x => x.SubGenreId,
+                        principalTable: "SubGenres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artists_GenreId",
+                table: "Artists",
+                column: "GenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artists_MusicId",
+                table: "Artists",
+                column: "MusicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artists_SubGenreId",
+                table: "Artists",
+                column: "SubGenreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Artists");
+        }
+    }
+}
