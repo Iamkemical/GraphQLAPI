6ed44e3bed21c97ecbe20a281ccf9e686c61f6d0
Gabriel Isaac <iamkemical1@gmail.com>
2021-02-24 12:49:20 +0100

    Added Artist Model

---
+++ src/MusicApp.Domain/Models/Artist.cs
@@ -0,0 +1,36 @@
+﻿using MusicApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MusicApp.Domain.Models
+{
+    public class Artist
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+        [ForeignKey("GenreId")]
+        public virtual Genre Genre { get; set; }
+
+        [Required]
+        public int SubGenreId { get; set; }
+
+        [ForeignKey("SubGenreId")]
+        public virtual SubGenre SubGenre { get; set; }
+
+        [Required]
+        public int MusicId { get; set; }
+
+        [ForeignKey("MusicId")]
+        public virtual Music Music { get; set; }
+    }
+}
